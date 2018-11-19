33522482304bb0c9e84d071cc428ef5b02106b95
Sami Eriksson <sami.eriksson@yahoo.fi>
2018-11-19 09:25:13 +0200

    Projektin alustus. Gitignorea muutettu.

---
+++ src/main/java/fi/loppuprojekti/back/LoppuprojektiBackApplication.java
@@ -0,0 +1,12 @@
+package fi.loppuprojekti.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoppuprojektiBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoppuprojektiBackApplication.class, args);
+    }
+}
